8fcd9d6000ccdced80838d138a9652c8107919db
Michelle Melton <meltonml@appstate.edu>
2021-07-06 15:28:19 -0400

    Update user notification text

--- lang/en/local_userbackupdelete.php
+++ lang/en/local_userbackupdelete.php
@@ -30,4 +30,4 @@ $string['getfiles'] = 'Get user backup files to delete';
 $string['deletefiles'] = 'Delete user backup files';
 $string['notifyusers'] = 'Notify users of backup files to be deleted';
 $string['messagesubject'] = 'Files in user private backup area to be deleted';
-$string['messagebody'] = '<p>The following files in your user private backup area are over 30 days old. They will be permanently deleted {$a->deletedate}.</p> {$a->filelist}';
+$string['messagebody'] = '<p>The following files in your user private backup area are over 30 days old. They will be permanently deleted {$a->deletedate}.</p> {$a->filelist} <p>To learn more about user private backup files and how to access and download them, view <a href="https://confluence.appstate.edu/x/aIYxBw">Accessing User Backups</a> in the ATKB.</p>';
